57a196d80e6af0d7b45211c0df849276ccc53da7
D.Edwards <David.Edwards.Sr05@gmail.com>
2026-06-28 00:12:55 -0400

    ALAB 351.1 lab script

---
+++ first_python.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+name = input("What is your name? ")
+if name == "":
+	name = "stranger"
+print("Hello, " + name + "! Glad to have you learning Python.")
