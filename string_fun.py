4ede265924301c4b5dc94a57615a80179b10a2a3
D.Edwards <David.Edwards.Sr05@gmail.com>
2026-07-06 23:26:02 -0400

    created fun simple string script

---
+++ string_fun.py
@@ -0,0 +1,12 @@
+#David Edwards
+#2026-CAX-176
+#Lab 351.2 - string_fun.py
+
+#This program will take a string from the user and perform various operations on it.
+word = input('Enter a word. Whatever you do, don\'t say Candyman: ')
+
+#Use len() to count characters
+print(f'Your word has {len(word)} letters.')
+print(word.upper())
+print(word *3)
+print('Be my victim')
