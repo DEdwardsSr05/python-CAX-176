ecc4dfb5409166709e99de44b26dc3e4e312a6da
D.Edwards <David.Edwards.Sr05@gmail.com>
2026-07-06 21:20:40 -0400

    Updated types_and_vars.py with age and rectangle calculations

--- types_and_vars.py
+++ types_and_vars.py
@@ -15,3 +15,14 @@ height = float(height_in_inches / 39.3701)
 
 # Introduction
 print('Hello my name is ' + name + '. I am ' + str(age) + ' years of age and my height is ' + str(round(height, 2)) + ' meters.')
+
+#Add 5 years to my age and print the age in the future
+age_in_5_years = age + 5
+print('In 5 years I will be ' + str(age_in_5_years) + ' years old.')
+
+#Calculate the area of a rectangle with the width of 5.5 meters and height of 2 meters. Print the area.
+width = 5.5
+length_of_rectangle = 2
+area = width * length_of_rectangle
+print('I am trying to figure out the the area of a rectangle for my garden. The width is ' + str(width) + ' meters and the length is ' + str(length_of_rectangle) + ' meters. This is small for a garden, but its a start.')
+print('The area is ' + str(area) + ' m^2.')
