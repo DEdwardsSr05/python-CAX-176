557247ecf040722a49aee14d5b59f3a7abf9c728
D.Edwards <David.Edwards.Sr05@gmail.com>
2026-07-10 21:04:47 -0400

    Script to check if a user has WRITE permission

--- permission_matrix_bitwise.py
+++ permission_matrix_bitwise.py
@@ -1,1 +1,17 @@
-# 
+# Script to check user permissions 
+# Binary as integers
+
+READ = 4     # Binary 100
+WRITE = 2    # Binary 010
+EXECUTE = 1  # Binary 001
+
+# Ask for surrent user permission
+user_permission = int(input('What is current permission? Enter 1-7: '))  # Binary 010
+
+print('*****Bitwise Testing*****')
+
+if (user_permission & WRITE) == WRITE:
+    print('Success, user has WRITE permission')
+
+else:
+    print('User does not have permission for WRITE')
